1cdc1a0a15f01831a1b4241324a6eaddbfe6ea63
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-08-17 00:13:44 +0530

    latest one

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -3,8 +3,6 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
-
-import { SeedCommand } from '../src/seed/seed.command';
 import { BannerSong } from './songs/recentplay';
 @Module({
  imports : [TypeOrmModule.forFeature([User, BannerSong])],
